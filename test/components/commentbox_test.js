82dc4e478319b9acfd91d8eb9a960cefd1c6143c
Aissam Salhi <aissam.salhi@dst1032.local>
2017-07-06 19:43:32 +0200

    Added first test structure for commentbox component

---
+++ test/components/commentbox_test.js
@@ -0,0 +1,17 @@
+import {renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/CommentBox';
+
+describe("Comment box tests", () => {
+	it("should contain a text area" , () => {
+		const commentBoxRendered = renderComponent(CommentBox);
+		
+	});
+
+	it("should have a button", () => {
+
+	});
+
+	it("should clear the text area when button is pressed", () => {
+
+	});
+})
