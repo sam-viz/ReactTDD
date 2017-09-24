a3cb82cda20d37dc4a58349b31de9672254ae894
Aissam Salhi <ais.salhy@gmail.com>
2017-09-25 00:30:55 +0200

    Added Unit Tests for the comment box class

--- test/components/comment-box_test.js
+++ test/components/comment-box_test.js
@@ -22,9 +22,34 @@ describe("Comment box Component", () => {
         expect(commentBox.find("button")).to.exist;
     });
 
-    describe("Behavior", () => {
+
+    describe("Comment Text Area Behavior", () => {
+        let inputValue;
+        
+        beforeEach(() => {
+            inputValue="Hey ! I liked your article :)";
+        });
+        
+        it("The textarea should contain the input value" , () => {
+            commentBox.find("textarea").simulate("change", inputValue);
+            expect(commentBox.find("textarea")).to.have.value(inputValue);
+        });
+        
+    });
+        
+    describe("Component Behavior", () => {
+        let inputValue;
+       
+        beforeEach(() => {
+            inputValue="I very much have liked your post ! Thank you ";
+            commentBox.find("textarea").simulate("focus");
+            commentBox.find("textarea").simulate("change", inputValue);
+        });
+
         it("should clear the textarea when the text is submitted", () => {
-            
+            commentBox.find("form").simulate("submit");
+            expect(commentBox.find("textarea")).to.have.value("");
         });
     });
+    
 });
