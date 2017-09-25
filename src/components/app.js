66e518ae4a79fec8e0fc30283987c511b3736c82
Aissam Salhi <ais.salhy@gmail.com>
2017-09-25 02:58:24 +0200

    Added Comment List Component to App and added some app tests

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CommentBox from "../containers/comment-box";
+import CommentList from "../containers/comment-list";
 
 export default class App extends Component {
     render() {
         return (
             <div className="container">
                 <CommentBox />
+                <CommentList />
             </div>
         );
     }
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,14 +3,57 @@ import { renderComponent, expect} from "../test_helper";
 import App from "../../src/components/app"; 
 
 describe("App main component", () => {
+
     let appComponent;
 
-    beforeEach(()=> {
-        appComponent=renderComponent(App);
+    describe("App main component without comment" , () => {
+
+        beforeEach(()=> {
+            appComponent=renderComponent(App);
+        });
+
+        it("should contain a container css class name", () => {
+            expect(appComponent).to.have.class("container");
+        });
+    
+        it("should contain a comment box component", () => {
+            expect(appComponent.find(".comment-box")).to.exist;
+        });
+
+
+        
+        it("should not contain a comment list component", () => {
+            expect(appComponent.find(".comment-list")).not.to.exist;
+        });
     });
 
-    it("should contain a container css class name", () => {
-        expect(appComponent).to.have.class("container");
+    describe("App main component with some comment", () => {
+        const props= { 
+            comments: [
+                {
+                    author: "Jaques",
+                    body : "This is a comment from Jaques"
+                },
+                {
+                    author: "Clémentine",
+                    body : "This is a comment from Clémentine"
+                },
+                {
+                    author: "Julien",
+                    body: "This is a comment from Julien"
+                }
+            ]
+        };
+       
+       
+        beforeEach(()=> {
+            appComponent=renderComponent(App,null, props);
+        });
+
+        it("should contain a comment list component", () => {
+            expect(appComponent.find(".comment-list")).to.exist;
+        });
+
     });
 
 });
